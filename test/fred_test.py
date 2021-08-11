3fffdb0ac195a482638b3ebb4196e43f7fc04e7b
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-08-11 18:16:40 -0400

    Pytest

---
+++ test/fred_test.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+
+from app.fred import to_usd, func
+
+CI_ENV = os.getenv("CI") == "true"
+
+@pytest.mark.skipif(CI_ENV == True, reason="to avoid issuing HTTP requests on the CI server")
+def test_func():
+    # with valid state_id
+    results = func(state_id="CA")
+    assert results[1] == "Positive"
+
+    # with invalid state_id, fails gracefully and returns nothing:
+    invalid_results = func(state_id="CAS")
+    assert invalid_results == None
+
+def test_to_usd():
+    assert to_usd("219999") == "$219,999"
